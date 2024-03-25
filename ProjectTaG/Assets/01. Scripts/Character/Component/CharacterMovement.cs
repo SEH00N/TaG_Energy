a5a0bcd4e52445d4f4340fa7260970a6a74d62f7
SEH00N <06psh0501@gmail.com>
2024-03-25 11:15:16 +0900

    character movement

---
+++ ProjectTaG/Assets/01. Scripts/Character/Component/CharacterMovement.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMovement : MonoBehaviour
+{
+    [Header("Gravity")]
+    [SerializeField] float gravityScale = 1f;
+
+    [Header("Ground Check")]
+    [SerializeField] LayerMask groundLayer = 0;
+    [SerializeField] float groundThreshold = 0.5f;
+
+    private bool isGround = false;
+    private Vector3 velocity = Vector3.zero;
+    public Vector3 Velocity => velocity;
+
+    private CharacterController characterController = null;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+	private void FixedUpdate()
+    {
+        isGround = characterController.isGrounded | CheckGround();
+        if(isGround && velocity.y < 0f)
+            ApplyGravity(Time.fixedDeltaTime, 0.3f);
+        else
+            ApplyGravity(Time.fixedDeltaTime, 1f);
+
+        ApplyVelocity(Time.fixedDeltaTime);
+    }
+
+    public void SetVelocity(Vector3 velocity)
+    {
+        this.velocity = velocity;
+    }
+
+    private bool CheckGround()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, groundThreshold, groundLayer);
+    }
+
+    private void ApplyGravity(float deltaTime, float scale = 1f)
+    {
+        velocity.y += Physics.gravity.y * gravityScale * deltaTime * scale;
+    }
+
+    private void ApplyVelocity(float deltaTime)
+    {
+        characterController.Move(velocity * deltaTime);
+    }
+}
